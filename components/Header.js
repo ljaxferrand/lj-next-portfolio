ab009c4dc25877dcee0ac9cfe476a02e0bcc5374
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-06-28 13:01:04 -0700

    Add site logo and menu json

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import globalData from "../utilities/globalData.preval";
 import Link from "next/link";
 
 const Header = () => {
   return (
     <header>
+      <Link href="/">
+        <a>
+          <img
+            className="p-4 w-24"
+            src={globalData.acf.logo.url}
+            alt={globalData.acf.logo.url}
+          />
+        </a>
+      </Link>
+
       <nav>
         <ul>
           <li>

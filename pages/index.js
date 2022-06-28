a6f306e49690a71f90e7d9a033aeeda27c1c6354
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-06-28 14:01:19 -0700

    Load custom fonts

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ const Home = () => {
 
   return (
     <div className="bg-teal-300">
-      <h1 className="text-3xl font-bold underline">Hello {name}!</h1>
+      <h1 className="text-3xl underline">Hello {name}!</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      heading: ["Rokkitt", "serif"],
+    },
+    screens: {
+      xs: "320px",
+      s: "485px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      colors: {
+        primaryAccent: "#2EC4B6",
+        offBlack: "#1F0322",
+        offWhite: "#FFFAFA",
+      },
+    },
   },
   plugins: [],
 };

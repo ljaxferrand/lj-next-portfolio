92be05a42950c55be6eec26389a52640b73968b6
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-07-02 14:31:24 -0700

    Add responsive ProjectList card styles

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -1,16 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 const ProjectList = ({ data }) => {
   return (
     <ul className="container mx-auto">
       {data.map((project) => (
-        <li key={project.id} className="lg:flex justify-center group py-8">
+        <motion.li
+          key={project.id}
+          className="lg:flex justify-center group py-8"
+        >
           <Link href={`/work/?id=${project.id}`} as={`/work/${project.id}`}>
-            <a className="flex flex-col items-center lg:items-start lg:flex-row gap-6">
+            <a className="flex flex-col items-center lg:items-start lg:flex-row gap-12">
               <div className="w-96 h-96 shadow-2xl rounded-md overflow-hidden lg:group-odd:order-2">
                 <img
-                  className="w-96 h-96 object-cover"
+                  className="w-96 h-96 object-cover group-even:bg-yellowBG group-odd:bg-primaryAccent"
                   src={project.acf.project_featured_img.url}
                   alt={
                     project.acf.project_featured_img.alt ||
@@ -19,14 +23,16 @@ const ProjectList = ({ data }) => {
                 />
               </div>
 
-              <div className="relative lg:py-8  max-w-md">
+              <div className="relative max-w-md lg:py-8 lg:mt-8 ">
                 <h2>{project.acf.project_title}</h2>
-                <h3 className="absolute ">{project.acf.type}</h3>
+                <h3 className="absolute -top-4 lg:top-4 left-6">
+                  {project.acf.type}
+                </h3>
                 <p className="">{project.acf.excerpt}</p>
               </div>
             </a>
           </Link>
-        </li>
+        </motion.li>
       ))}
     </ul>
   );

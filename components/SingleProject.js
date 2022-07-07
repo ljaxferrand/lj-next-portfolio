0632578815823b69b8fe786ef5e487313d520e22
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-07-07 12:58:09 -0700

    Add Cover block comp for SingleProject

--- components/SingleProject.js
+++ components/SingleProject.js
@@ -1,13 +1,17 @@
+import Cover from "./Cover";
 import Pagination from "./Pagination";
 
 const SingleProject = ({ workInfo, projectPagination }) => {
   return (
     <div id="main-content">
-      <h1>{workInfo.acf.project_title}</h1>
-      <h2>{workInfo.acf.type}</h2>
-      <p>{workInfo.acf.excerpt}</p>
+      <Cover
+        title={workInfo.acf.project_title}
+        description={workInfo.acf.excerpt}
+      />
 
-      {/* Styles for this are set via "./styles/globals.css" */}
+      {/* <h2>{workInfo.acf.type}</h2> */}
+
+      {/* Styles for the dangerousHTML are set via "./styles/globals.css" */}
       <div
         id="post-content"
         dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content }}

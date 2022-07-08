68fe66bc37a6a89548ce0cd43ae822dd3b6b4850
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-07-07 17:02:10 -0700

    Add svg wave divider to Cover comp

--- components/Cover.js
+++ components/Cover.js
@@ -1,19 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
+import WaveDivider from "./WaveDivider";
+
 const Cover = ({ title, description }) => {
   return (
-    <section className="text-white bg-primaryDarker">
-      <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-60 lg:items-center lg:flex ">
-        <div className="max-w-3xl mx-auto text-center">
-          <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-teal-300 via-blue-500 to-purple-600">
-            {title}
-          </h1>
+    <>
+      <section className="text-white bg-primaryDarker">
+        <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-60 lg:items-center lg:flex ">
+          <div className="max-w-3xl mx-auto text-center">
+            <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-teal-300 via-blue-500 to-purple-600">
+              {title}
+            </h1>
 
-          <p className="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl">
-            {description}
-          </p>
+            <p className="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl">
+              {description}
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <WaveDivider />
+    </>
   );
 };
 

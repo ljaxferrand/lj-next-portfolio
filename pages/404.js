f54d1d35d5e497c2e718400be887fdfce9500d6e
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-07-08 16:31:32 -0700

    Add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
+import Cover from "../components/Cover";
+
+const Custom404 = () => {
+  return (
+    <>
+      <div id="main-content">
+        <Cover title={"Error: 404"} description={"Page not Found!"} />
+        <div className="container mx-auto flex flex-col lg:flex-row justify-center items-center">
+          <p className="max-w-md p-4 text-3xl">
+            I might have fallen asleep before publishing the page you are
+            looking for. Try heading back to the home page!
+          </p>
+          <img
+            className="px-4 pb-12"
+            src="../images/sleeping-at-the-desk.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <aside className="text-center p-4">
+        Illustration by{" "}
+        <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">
+          Icons 8
+        </a>{" "}
+        from <a href="https://icons8.com/illustrations">Ouch!</a>
+      </aside>
+    </>
+  );
+};
+
+export default Custom404;

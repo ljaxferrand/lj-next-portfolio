986100fc5ef888c4f4b69187c12614cc80364df7
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-07-08 17:59:32 -0700

    Change breakpoint for text size

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ const Custom404 = () => {
       <div id="main-content">
         <Cover title={"Error: 404"} description={"Page not Found!"} />
         <div className="container mx-auto flex flex-col lg:flex-row justify-center items-center">
-          <p className="max-w-md p-4 text-3xl lg:order-2">
+          <p className="max-w-lg p-4 s:text-3xl lg:order-2">
             I might have fallen asleep before publishing the page you are
             looking for. Try heading back to the home page!
           </p>

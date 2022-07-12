9c5f5e2567684428112dba61370492329a1372a5
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-07-12 13:04:14 -0700

    Add dynamic content sections for Projects

--- components/SingleProject.js
+++ components/SingleProject.js
@@ -8,10 +8,56 @@ const SingleProject = ({ workInfo, projectPagination }) => {
         title={workInfo.acf.project_title}
         description={workInfo.acf.excerpt}
       />
-      <div
-        id="post-content"
-        dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content }}
-      ></div>
+      {/* Required Project Sections */}
+      <div className="container mx-auto">
+        <h2 className="text-center">{workInfo.acf.content_heading_1}</h2>
+        <div
+          className="post-content"
+          dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content_1 }}
+        ></div>
+
+        <h2 className="text-center">{workInfo.acf.content_heading_2}</h2>
+        <div
+          className="post-content"
+          dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content_2 }}
+        ></div>
+
+        <h2 className="text-center">{workInfo.acf.content_heading_3}</h2>
+        <div
+          className="post-content"
+          dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content_3 }}
+        ></div>
+
+        <h2 className="text-center">{workInfo.acf.content_heading_4}</h2>
+        <div
+          className="post-content"
+          dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content_4 }}
+        ></div>
+
+        {/* Optional Project Sections */}
+        {workInfo.acf.project_content_5 && (
+          <>
+            <h2 className="text-center">{workInfo.acf.content_heading_5}</h2>
+            <div
+              className="post-content"
+              dangerouslySetInnerHTML={{
+                __html: workInfo.acf.project_content_5,
+              }}
+            ></div>
+          </>
+        )}
+        {workInfo.acf.project_content_6 && (
+          <>
+            <h2 className="text-center">{workInfo.acf.content_heading_6}</h2>
+            <div
+              className="post-content"
+              dangerouslySetInnerHTML={{
+                __html: workInfo.acf.project_content_6,
+              }}
+            ></div>
+          </>
+        )}
+      </div>
       <Pagination
         current={workInfo.id}
         prev={workInfo.previous}

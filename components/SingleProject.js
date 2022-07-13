681e7c7b4d8a6afe0e996f87fa56e29634a22cfe
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-07-12 18:37:21 -0700

    Add Scrollspy nav in SingleProject

--- components/SingleProject.js
+++ components/SingleProject.js
@@ -1,5 +1,10 @@
+import dynamic from "next/dynamic";
 import Cover from "./Cover";
 import Pagination from "./Pagination";
+
+const ScrollSpyNav = dynamic(() => import("react-scrollspy-nav"), {
+  ssr: false,
+});
 
 const SingleProject = ({ workInfo, projectPagination }) => {
   return (
@@ -9,54 +14,115 @@ const SingleProject = ({ workInfo, projectPagination }) => {
         description={workInfo.acf.excerpt}
       />
       {/* Required Project Sections */}
-      <div className="container mx-auto">
-        <h2 className="text-center">{workInfo.acf.content_heading_1}</h2>
-        <div
-          className="post-content"
-          dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content_1 }}
-        ></div>
 
-        <h2 className="text-center">{workInfo.acf.content_heading_2}</h2>
-        <div
-          className="post-content"
-          dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content_2 }}
-        ></div>
+      <div className="container mx-auto relative">
+        <div className="sticky top-24 left-0 pl-8 max-w-content">
+          <ScrollSpyNav
+            scrollTargetIds={[
+              "section_1",
+              "section_2",
+              "section_3",
+              "section_4",
+              "section_5",
+              "section_6",
+            ]}
+            offset={-100}
+            scrollDuration="500"
+            activeNavClass="scrollspy-is-active"
+          >
+            <p>Table of Contents</p>
+            <ul>
+              <li>
+                <a href="#section_1">{workInfo.acf.content_heading_1}</a>
+              </li>
+              <li>
+                <a href="#section_2">{workInfo.acf.content_heading_2}</a>
+              </li>
+              <li>
+                <a href="#section_3">{workInfo.acf.content_heading_3}</a>
+              </li>
+              <li>
+                <a href="#section_4">{workInfo.acf.content_heading_4}</a>
+              </li>
+              {workInfo.acf.project_content_5 && (
+                <li>
+                  <a href="#section_5">{workInfo.acf.content_heading_5}</a>
+                </li>
+              )}
+              {workInfo.acf.project_content_6 && (
+                <li>
+                  <a href="#section_6">{workInfo.acf.content_heading_6}</a>
+                </li>
+              )}
+            </ul>
+          </ScrollSpyNav>
+        </div>
 
-        <h2 className="text-center">{workInfo.acf.content_heading_3}</h2>
-        <div
-          className="post-content"
-          dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content_3 }}
-        ></div>
-
-        <h2 className="text-center">{workInfo.acf.content_heading_4}</h2>
-        <div
-          className="post-content"
-          dangerouslySetInnerHTML={{ __html: workInfo.acf.project_content_4 }}
-        ></div>
-
-        {/* Optional Project Sections */}
-        {workInfo.acf.project_content_5 && (
-          <>
-            <h2 className="text-center">{workInfo.acf.content_heading_5}</h2>
+        <div className="">
+          <section id="section_1">
+            <h2 className="text-center">{workInfo.acf.content_heading_1}</h2>
             <div
               className="post-content"
               dangerouslySetInnerHTML={{
-                __html: workInfo.acf.project_content_5,
+                __html: workInfo.acf.project_content_1,
               }}
             ></div>
-          </>
-        )}
-        {workInfo.acf.project_content_6 && (
-          <>
-            <h2 className="text-center">{workInfo.acf.content_heading_6}</h2>
+          </section>
+
+          <section id="section_2">
+            <h2 className="text-center">{workInfo.acf.content_heading_2}</h2>
             <div
               className="post-content"
               dangerouslySetInnerHTML={{
-                __html: workInfo.acf.project_content_6,
+                __html: workInfo.acf.project_content_2,
               }}
             ></div>
-          </>
-        )}
+          </section>
+
+          <section id="section_3">
+            <h2 className="text-center">{workInfo.acf.content_heading_3}</h2>
+            <div
+              className="post-content"
+              dangerouslySetInnerHTML={{
+                __html: workInfo.acf.project_content_3,
+              }}
+            ></div>
+          </section>
+
+          <section id="section_4">
+            <h2 className="text-center">{workInfo.acf.content_heading_4}</h2>
+            <div
+              className="post-content"
+              dangerouslySetInnerHTML={{
+                __html: workInfo.acf.project_content_4,
+              }}
+            ></div>
+          </section>
+
+          {/* Optional Project Sections */}
+          {workInfo.acf.project_content_5 && (
+            <section id="section_5">
+              <h2 className="text-center">{workInfo.acf.content_heading_5}</h2>
+              <div
+                className="post-content"
+                dangerouslySetInnerHTML={{
+                  __html: workInfo.acf.project_content_5,
+                }}
+              ></div>
+            </section>
+          )}
+          {workInfo.acf.project_content_6 && (
+            <section id="section_6">
+              <h2 className="text-center">{workInfo.acf.content_heading_6}</h2>
+              <div
+                className="post-content"
+                dangerouslySetInnerHTML={{
+                  __html: workInfo.acf.project_content_6,
+                }}
+              ></div>
+            </section>
+          )}
+        </div>
       </div>
       <Pagination
         current={workInfo.id}

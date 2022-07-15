30b1990c4d23c775f38754c39774ac67a7bb8884
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-07-14 18:18:08 -0700

    Pull about page data

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,32 @@
+import axios from "axios";
 import Cover from "../components/Cover";
 
-const About = () => {
+const About = ({ aboutData }) => {
   return (
     <div id="main-content">
-      <Cover title={"About"} description={"Tinker Coder Creative Try"} />
+      <Cover
+        title={aboutData.acf.title}
+        description={"Tinker Coder Creative Try"}
+      />
+
+      <div
+        className="post-content"
+        dangerouslySetInnerHTML={{
+          __html: aboutData.acf.bio_text,
+        }}
+      ></div>
     </div>
   );
 };
 export default About;
+
+export async function getStaticProps() {
+  const responseAbout = await axios.get(
+    "https://staging6.ljferrand.com/wp-json/wp/v2/pages/346?_fields=acf&acf_format=standard"
+  );
+  const aboutData = responseAbout.data;
+
+  return {
+    props: { aboutData },
+  };
+}

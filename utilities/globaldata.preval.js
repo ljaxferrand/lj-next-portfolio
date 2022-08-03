aae5b8e807ee2572bdad7a3e3b3875d42662e0e9
LJ Ferrand <87734454+ljaxferrand@users.noreply.github.com>
2022-08-03 13:56:52 -0700

    Add .env var to preval plugin function

--- utilities/globaldata.preval.js
+++ utilities/globaldata.preval.js
@@ -1,8 +1,11 @@
 import preval from "next-plugin-preval";
 
+// Layout children (like header, footer) use data pulled from the CMS, but Next.js currently doesn't allow getStaticProps for non-page components.
+// This plugin fetches this global data for now.
+
 async function getGlobalData() {
   const res = await fetch(
-    `https://staging6.ljferrand.com/wp-json/wp/v2/pages/331?_fields=acf&acf_format=standard`
+    `${process.env.CMS_URL}/wp-json/wp/v2/pages/331?_fields=acf&acf_format=standard`
   );
 
   const globalData = res.json();
